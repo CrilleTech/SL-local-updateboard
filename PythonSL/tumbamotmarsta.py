4e8ad160816d327c6eaea63a9ea48609ee9f6354
CrilleTech <43004135+CrilleTech@users.noreply.github.com>
2018-09-11 12:04:47 +0200

    Update tumbamotmarsta.py

--- PythonSL/tumbamotmarsta.py
+++ PythonSL/tumbamotmarsta.py
@@ -1,6 +1,7 @@
 import requests
+
 def TumbaMotMarsta():
-    r = requests.get("http://api.sl.se/api2/realtimedeparturesV4.json?key=f96d6c1868c4430a9d631ef0d1c7701b&siteid=9524&timewindow=60")
+    r = requests.get("http://api.sl.se/api2/realtimedeparturesV4.json?key=*your-api-key-here*&siteid=9524&timewindow=60")
     j = r.json()
     print(r.status_code)
     trains = j['ResponseData']['Trains']
