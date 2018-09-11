842aa49659382b4ec9a928ff013adebfb7ea3fd7
CrilleTech <43004135+CrilleTech@users.noreply.github.com>
2018-09-11 12:03:01 +0200

    Update segersjomottumba.py

--- PythonSL/segersjomottumba.py
+++ PythonSL/segersjomottumba.py
@@ -1,12 +1,14 @@
-import requests
+#
 
+import requests
+#request lib to read the URI in Json
 def SegersjoMotTumba():
-    r = requests.get("http://api.sl.se/api2/realtimedeparturesV4.json?key=f96d6c1868c4430a9d631ef0d1c7701b&siteid=7263&timewindow=60")
+    r = requests.get("http://api.sl.se/api2/realtimedeparturesV4.json?key="-youre-api-key-here-"&siteid=7263&timewindow=60")
     j = r.json()
-    print(r.status_code)
+    print(r.status_code) #mostly for debugging
     buses = j['ResponseData']['Buses']
     for bus in buses:
-        if bus['JourneyDirection'] == 2:
-            print(bus['Destination'] + " : " + bus['DisplayTime'])
+        if bus['JourneyDirection'] == 2:  #journey direction is either 1 or 2 depending on which way the buss travels. 
+            print(bus['Destination'] + " : " + bus['DisplayTime']) #print out the buss destination from the bus stop as defined in the siteid in he URI above
 
 
